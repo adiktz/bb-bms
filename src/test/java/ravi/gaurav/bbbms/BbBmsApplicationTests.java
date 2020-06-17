68e8727aa6df4fa1a6eba32d3dd7a29fc59781ac
gaurav <gauravraviji@gmail.com>
2020-06-18 00:33:08 +0530

    First Commit: Starting with gradle

---
+++ src/test/java/ravi/gaurav/bbbms/BbBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi.gaurav.bbbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbBmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
